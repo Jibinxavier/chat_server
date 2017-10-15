efc0c6d513759dc96f60122e2101ab2846f1d136
Jibin Xavier <xavierj@tcd.ie>
2017-10-15 14:22:48 +0100

    created data structures for representing state

--- main.go
+++ main.go
@@ -1,40 +1,68 @@
 package main
 
-import "net"
-import "fmt"
-//import "bufio"
-import "strings" // only needed below for sample processing
-//import "io/ioutil"
-import "os"
+import (
+    "net"
+    "fmt"
+    "strings"
+    "os"
+)
+func chatMessage(rRef, name, mesg string) (string) {
+	return fmt.Sprintf("CHAT: {}\nCLIENT_NAME: {}\nMESSAGE: {}\n\n")
+}
+type Client struct {
+	conn		net.Conn
+	addr		string
+	uid			string
+	name		string
+
+}
+type Mesg struct {
+    mesgFrom 	string
+	chatId 		string
+	clientName 	string
+	mesg 		string
+}
+
+type ChatRoom struct {
+	id			string
+	name 		string
+	users		map[string][]Client
+}
+
+
 func main() {
 
-  fmt.Println("Launching server...")
+    fmt.Println("Launching server...")
 
-  // listen on all interfaces
-  ln, _ := net.Listen("tcp", ":8080")
+    // listen on all interfaces
+    ln, _ := net.Listen("tcp", ":8080")
 
-  // accept connection on port
-  conn, _ := ln.Accept()
+    // accept connection on port
+    conn, _ := ln.Accept()
+    var client = Client {conn,"testaddr","testuid","testname"}
+    fmt.Print("client struc "+ client.name)
+    // run loop forever (or until ctrl-c)
+    for {
+        // will listen for message to process ending in newline (\n)
+        var buf = make([]byte, 1024)
 
-  // run loop forever (or until ctrl-c)
-  for {
-    // will listen for message to process ending in newline (\n)
-    var buf = make([]byte, 1024)
-
-    mesgLen, err := conn.Read(buf)
+        mesgLen, err := conn.Read(buf)
 
 
-    if mesgLen ==0 {
-        fmt.Printf("Connection closed by remote host\n")
+        if mesgLen ==0 {
+            fmt.Printf("Connection closed by remote host\n")
+        }
+        checkError(err)
+        // output message received
+        fmt.Print("Message Received:", string(buf))
+        // sample process for string received
+        newmessage := strings.ToUpper(string(buf))
+        // send new string back to client
+        conn.Write([]byte(newmessage + "\n"))
     }
-    checkError(err)
-    // output message received
-    fmt.Print("Message Received:", string(buf))
-    // sample process for string received
-    newmessage := strings.ToUpper(string(buf))
-    // send new string back to client
-    conn.Write([]byte(newmessage + "\n"))
-  }
+
+  
+
 }
 func checkError(err error) {
     if err != nil {
